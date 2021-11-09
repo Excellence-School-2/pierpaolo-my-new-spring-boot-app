02f47ee713b3ec11677569fd7aacda38c5d33b77
PierpaoloFerrigno <pierpaolo.ferrigno@gmail.com>
2021-11-09 10:19:54 +0100

    codice base

---
+++ hellovscodeinitializr/src/main/java/it/ntt/hellovscodeinitializr/HellovscodeinitializrApplication.java
@@ -0,0 +1,13 @@
+package it.ntt.hellovscodeinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellovscodeinitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellovscodeinitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/ntt/hellospringboot/HellospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ntt.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
